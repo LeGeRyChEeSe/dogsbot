fb73e2f4457813279a9786bf77e120ff2e57a4a5
LeGeRyChEeSe <kilian.douarinou41@gmail.com>
2020-09-01 22:25:18 +0200

    Restructuration complète du bot

--- events/client/on_ready.py
+++
@@ -1,4 +0,0 @@
-async def on_ready(self):
-    print("Loggé en tant que")
-    print(self.user.name)
-    print(self.user.id)
--- main.py
+++ main.py
@@ -1,99 +1,56 @@
 import discord
-import random
+from discord.ext import commands
 import env
-import events.client.on_ready
-import events.client.on_message
+import random
+
+client = commands.Bot(command_prefix="!")
 
 
-class Bot(discord.Client):
-    def __init__(self):
-        super().__init__()
-
-    async def on_ready(self):
-        print(self.user.name + " loggé !")
-        print("Id : " + str(self.user.id))
-        print("En attente...")
-
-    def random_colour(self):
-        hexa = "0123456789abcdef"
-        random_colour = "0x"
-        for i in range(6):
-            random_colour += random.choice(hexa)
-        return discord.Colour(int(random_colour, 16))
-
-    def create_embed(self, title, description, colour, image=None):
-        embed = discord.Embed()
-        embed.title = title
-        embed.description = description
-        embed.colour = colour
-        if (image != None):
-            embed.set_thumbnail(url=image)
-        return embed
-
-    async def on_message(self, message):
-
-        server = message.guild
-        channel = message.channel
-        author = message.author
-        content = message.content
-
-        prefix = "!"
-
-        if (author == self.user):
-            return
-
-        if (content.startswith(prefix + "dm")):
-
-            try:
-                name = content.split(" ")[1]
-            except:
-                return await channel.send("Aucun utilisateur.")
-
-            member = discord.utils.get(
-                server.members, name=name)
-
-            try:
-                return await member.send("Je suis arrivé dans tes DMs !")
-            except discord.Forbidden:
-                return await member.send("L'utilisateur n'accepte pas les DMs depuis des membres de ce serveur !")
-
-        if (content.startswith(prefix + "em")):
-
-            title = author.display_name
-
-            roles = author.roles
-
-            roles.sort(reverse=True)
-
-            description = ""
-
-            for role in roles:
-                description += role.mention + "\n"
-
-            colour = self.random_colour()
-
-            image = author.avatar_url
-
-            embed = self.create_embed(title, description, colour, image)
-
-            return await channel.send(embed=embed)
-
-        if (content.startswith(prefix + "invite")):
-            invite = await channel.create_invite(max_age=3600, unique=False)
-
-            if (message.mentions == None):
-                return await channel.send(invite.url)
-            else:
-                for mention in message.mentions:
-                    await mention.send(
-                        "Vous avez reçu une invitation dans le serveur " + str(server) + " envoyée par " + invite.inviter.mention + " !\nElle est valable pour " + str(invite.max_age/3600) + "h !\n" + invite.url)
-                return await channel.send("Le lien d'invitation au serveur " + server.name + " a été envoyé à toutes les personnes mentionnées.")
-
-        if (content.startswith(prefix)):
-            await channel.send("La commande " + content + " n'existe pas encore, veuillez patienter !")
+@client.event
+async def on_ready():
+    print("Le bot est prêt.")
 
 
-if __name__ == '__main__':
+@client.event
+async def on_member_join(member):
+    print(f'{member} a rejoins le serveur.')
 
-    bot = Bot()
-    bot.run(env.TOKEN)
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} a quitté le serveur.')
+
+
+@client.command()
+async def ping(self):
+    await self.send(f"Pong! {round(client.latency * 1000)}ms")
+
+
+@client.command(aliases=['8ball', 'eightball'])
+async def _8ball(self, *, question):
+    responses = ["Bien sûr!", "Mais oui, t'a cru toi...", "Effectivement.", "Je ne te le fais pas dire!", "Ca m'étonnerait !", "T'es irrécupérable... :facepalm:", "Je te le donne dans le mille Émile !",
+                 "Faut arrêter la drogue mon vieux.", "Peut-être que oui, peut-être que non", "Et moi j'ai une question : Quelle est la différence entre un hamburger ? :thinking:"]
+    await self.send(f"{random.choice(responses)}")
+
+
+@client.command(aliases=['purge', 'erase', 'delete'])
+async def clear(self, amount=5):
+    await self.channel.purge(limit=amount + 1)
+
+
+@client.command()
+async def kick(self, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(self, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+
+@client.command()
+async def me(self, *, message="Veuillez entrer du texte pour que je puisse le répéter !"):
+    await self.message.delete()
+    await self.send(f"{message}")
+
+client.run(env.TOKEN)
