f97819d5a864341172e138b16d37f18dcf2017de
LeGeRyChEeSe <kilian.douarinou41@gmail.com>
2021-05-02 04:47:36 +0200

    delete files

--- run-bot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pgrep -f "/usr/bin/SCREEN -dmS dogsbot /home/kilian/.pyenv/versions/dogsbot/bin/python /home/kilian/bots/dogsbot/main.py"
-
-if [ $? -eq 1 ]
-then
-cd /home/kilian/bots/dogsbot/
-/usr/bin/screen -dmS dogsbot /home/kilian/.pyenv/versions/dogsbot/bin/python /home/kilian/bots/dogsbot/main.py
-fi
